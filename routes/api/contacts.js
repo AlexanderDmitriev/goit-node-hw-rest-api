2abd93198b749df45d9f5e8c1ef601b2de135827
AlexanderDmitriev <alex9110077@gmail.com>
2022-06-19 00:16:54 +0300

    start

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,43 @@
-const express = require('express')
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+const prevRes = require("../../models/contacts.json");
 
-const router = express.Router()
+/* Создаём новую "страницу" в сервере */
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  res.json({
+    status: "success",
+    code: 200,
+    data: { result: prevRes },
+  });
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = prevRes.find((contactItem) => contactItem.id === contactId);
+  if (!result) {
+    res.json({
+      status: "error",
+      code: 404,
+      message: `Contact with id = ${contactId} not found`,
+    });
+  }
+  res.json({ status: "success", code: 200, data: { result: result } });
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  const newContact = { ...req.body, id: uuidv4() };
+  prevRes.push(newContact);
+  res.json({ status: "adding", code: 201, data: { result: newContact } });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-module.exports = router
+module.exports = router;

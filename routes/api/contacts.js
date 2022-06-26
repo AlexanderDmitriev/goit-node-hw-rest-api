5025998369266e3907ae6ac2722901e4e4eec224
AlexanderDmitriev <alex9110077@gmail.com>
2022-06-26 13:57:31 +0300

    Done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,149 +1,19 @@
 const express = require("express");
-const {Contact,joiSchema} = require("../../models");
-/* const {addContact} = require('./addContact');
-const {ctrlWrapper} = require('./ctrlWrapper'); */
+const ctrl = require("../../controllers/contacts");
 
 /* Создаём новую "страницу" в сервере */
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  try {
-    const result = await Contact.find({});
-    if (!result) {
-      const error = new Error("Not found");
-      error.status = 404;
-      throw error;
-    }
-    res.json({
-      status: "success",
-      code: 200,
-      data: { result },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.get("/", ctrl.getAllContacts);
 
-/* router.get("/",ctrlWrapper(addContact)); */
+router.get("/:contactId", ctrl.getById);
 
-router.get("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await Contact.findById(contactId);
-    if (!result) {
-      const error = new Error("Not found");
-      error.status = 404;
-      throw error;
-    }
-    res.json({ status: "success", code: 200, data: { result } });
-  } catch (error) {
-    next(error);
-  }
-});
+router.post("/", ctrl.addContact);
 
-router.post("/", async (req, res, next) => {
-  try {
-    const valideResult = joiSchema.validate(req.body);
-    if (valideResult.error) {
-      return res.status(400).json({
-        status: "error",
-        code: 400,
-        message: valideResult.error.message,
-      });
-    };
-    const result = await Contact.create(req.body);
-    res.json({ status: "success", code: 201, data: { result } });
-  } catch (error) {
-    next(error);
-  }
-});
+router.put("/:contactId", ctrl.updateContact);
 
-router.delete("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const removingContact = await Contact.findByIdAndRemove(contactId);
-    if (!removingContact) {
-      res.status(404).json({
-        status: "error",
-        code: 404,
-        message: `Not found`,
-      });
-    }
-    res.json({
-      status: "success",
-      code: 200,
-      message: "contact deleted",
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.delete("/:contactId", ctrl.deleteContact);
 
-router.put("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    if (!req.body) {
-      res.status(400).json({
-        status: "error",
-        code: 400,
-        message: `missing fields`,
-      });
-    }
-    const valideResult = joiSchema.validate(req.body);
-    if (valideResult.error) {
-      return res.status(400).json({
-        status: "error",
-        code: 400,
-        message: valideResult.error.message,
-      });
-    }
-    const result = await Contact.findByIdAndUpdate(contactId, req.body);
-
-    if (!result) {
-      res.status(404).json({
-        status: "error",
-        code: 404,
-        message: `Not found`,
-      });
-    }
-    res.json({
-      status: "success",
-      code: 200,
-      data: { result },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.patch("/:contactId/favorite", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const { favorite } = req.body;
-    if (!req.body) {
-      res.status(400).json({
-        status: "error",
-        code: 400,
-        message: `missing field favorite`,
-      });
-    }
-    const result = await Contact.findByIdAndUpdate(contactId, { favorite });
-
-    if (!result) {
-      res.status(404).json({
-        status: "error",
-        code: 404,
-        message: `Not found`,
-      });
-    }
-    res.json({
-      status: "success",
-      code: 200,
-      data: { result },
-    });
-  } catch (error) {
-    next(error);
-  }
-});
+router.patch("/:contactId/favorite", ctrl.updateStatusContact);
 
 module.exports = router;

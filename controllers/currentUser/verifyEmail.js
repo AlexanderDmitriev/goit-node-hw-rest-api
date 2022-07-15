792fa4863dc8d31546ed95fac3d1f734015c326b
AlexanderDmitriev <alex9110077@gmail.com>
2022-07-15 15:02:05 +0300

    Update verifyEmail.js

--- controllers/currentUser/verifyEmail.js
+++ controllers/currentUser/verifyEmail.js
@@ -5,13 +5,13 @@ const verifyEmail = async (req, res) => {
   const { verificationToken } = req.params;
   const user = User.findOne({ verificationToken });
   if (!user) {
-    throw NotFound();
+    throw NotFound("User not found");
   }
   await User.findByIdAndUpdate(user._id, {
     verify: true,
     verificationToken: null,
   });
-  res.json({ message: "Verification has been susseccfully" });
+  res.json({ message: "Verification successful" });
 };
 
 module.exports = { verifyEmail };
